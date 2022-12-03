3eb265602f90ea6f7c81184e64bd4399c3effdb0
ashes2076 <ashwin.ganesh1505@gmail.com>
2022-12-03 16:03:31 +0100

    Day 2 WIP & Day 3 Part 1 & Part 2

--- Day3/Test.py
+++ Day3/Test.py
@@ -5,11 +5,20 @@ scores = dict([(j, i+1)for i, j in enumerate(ascii_letters)])
 with open('Day3\Input_Day3.txt', 'r') as f: 
     lines = [i.strip() for i in f.readlines()] 
 
+
+    ## PART 1 SUM OF THE PRIORITIES OF THOSE ITEM TYPES
     sum_priorities = 0
     for line in lines:
         a = line[:len(line)//2] # First Compartment
         b = line[len(line)//2:] # Second Compartment
-        char, = set(a) & set(b) # Comparin the 2 compartments and keeping the common one as a tuple 
+        char, = set(a) & set(b) # Comparing the 2 compartments and keeping the common one as a tuple 
         sum_priorities += scores[char]
-    print(f" sum of the priorities of those item types is : {sum_priorities}")
+    print(f"sum of the priorities of those item types is : {sum_priorities}")
 
+    ## PART 2   SUM OF PRIORITIES BADGES GROUP OF 3
+    sum_priorities = 0
+    combined =  zip(lines[::3], lines[1::3], lines[2::3])
+    for a, b, c in combined:
+        common_item, = set(a) & set(b) & set(c)
+        sum_priorities += scores[common_item]
+    print(f"sum of common items is : {sum_priorities}")
