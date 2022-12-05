abc3430677a37eae8de2513079985476bed17b08
ashes2076 <ashwin.ganesh1505@gmail.com>
2022-12-05 11:57:03 +0100

    "Day 5 commented"

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -13,12 +13,13 @@ stacks = parse_Stack(StackedCrate)
 
 part1, part2 = stacks[:], stacks[:]
 
+#looping the stacks with the moves
 for moves in RearrangementProcedure:    
-    _, Num_of_stacks, _, StartStack, _, EndStack = moves.split()
-    Num_of_stacks = int(Num_of_stacks); StartStack = int(StartStack); EndStack = int(EndStack)
+    _, Num_of_stacks, _, StartStack, _, EndStack = moves.split() #splitting moves into numbers
+    Num_of_stacks = int(Num_of_stacks); StartStack = int(StartStack); EndStack = int(EndStack) #converting them to int
 
-    part1[StartStack], part1[EndStack] = part1[StartStack][Num_of_stacks:], part1[StartStack][:Num_of_stacks][::-1] + part1[EndStack]
-    part2[StartStack], part2[EndStack] = part2[StartStack][Num_of_stacks:], part2[StartStack][:Num_of_stacks] + part2[EndStack] 
+    part1[StartStack], part1[EndStack] = part1[StartStack][Num_of_stacks:], part1[StartStack][:Num_of_stacks][::-1] + part1[EndStack] #reversing the arranged stack as crates are moved one by one
+    part2[StartStack], part2[EndStack] = part2[StartStack][Num_of_stacks:], part2[StartStack][:Num_of_stacks] + part2[EndStack] #modern machines takes multiple crates so need to reverse the rearrangement
 
 print(f"the rearranged stack is : {part1[1:]}")
 print("Part 1 :", "".join(a[0] for a in part1 if a))
